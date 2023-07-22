dc88654a71894e5902b72bf3f7d2d3f175c7f72c
Marius Popa <m.popa@1xinternet.de>
2023-07-22 22:02:23 +0200

    added useHistory

--- src/App.js
+++ src/App.js
@@ -5,10 +5,64 @@ function Square({ value, onSquareClick }){
   return <button className="square" onClick={onSquareClick}> {value} </button>
 }
 
+export default function Game() {
+  // const [xIsNext, setXIsNext] = useState(true); removed to line 14 because there is no need for both true and false in state
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+  const [currentMove, setCurrentMove] = useState(0); //keep track of which step the user is currently viewing
+  // const currentSquares = history[history.length - 1]; // rendering the final move
+  const currentSquares = history[currentMove] // render the currently selected move
+  const xIsNext = currentMove % 2 === 0;
 
-export default function Board() {
-  const [xIsNext, setXIsNext] = useState(true) // each time a player moves, the boolean will be filpped to determine which players goes next, saving tha state
-  const [squares, setSquares] = useState(Array(9).fill(null));
+
+
+    function handlePlay(nextSquares) {
+   // instead of setSquares to store the state, we using history state variable
+   //  setHistory([...history, nextSquares]); // create new array that contains all the history
+
+   // only keeping that portion of the old history.
+     const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
+     setHistory(nextHistory);
+    setCurrentMove(nextHistory.length - 1); //  update currentMove to point to the latest history entry.
+    // setXIsNext(!xIsNext); line 9
+    }
+
+      function jumpTo(nextMove) {
+        setCurrentMove(nextMove);
+       // line 9 setXIsNext(nextMove % 2 === 0); // set xIsNext to true if the number that you’re changing currentMove to is even.
+       }
+
+    const moves = history.map((squares, move) => { //move argument is the moves indexes
+    let description;
+    if (move > 0) {
+      description = 'Go to move #' + move;
+    } else {
+      description = 'Go to game start';
+    }
+    return (
+      <li key={move}>
+        <button onClick={() => jumpTo(move)}>{description}</button>
+      </li>
+    );
+  });
+
+
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
+      </div>
+      <div className="game-info">
+        <ol>{moves}</ol>
+      </div>
+    </div>
+  );
+}
+
+
+function Board({ xIsNext, squares, onPlay }) {
+// moved to parent Game and now used as props
+//  const [xIsNext, setXIsNext] = useState(true) // each time a player moves, the boolean will be filpped to determine which players goes next, saving tha state
+// const [squares, setSquares] = useState(Array(9).fill(null));
 
   // handle the state
   function handleClick(i){
@@ -25,8 +79,8 @@ export default function Board() {
       nextSquares[i] = "0";
     }
 
-    setSquares(nextSquares);
-    setXIsNext(!xIsNext);
+    onPlay(nextSquares);
+   //  setXIsNext(!xIsNext); // why to set as false
   }
 
   // let player know who is the winner or status of the moves
@@ -59,6 +113,8 @@ export default function Board() {
     </div>
   );
 }
+
+
 
 
 function calculateWinner(squares) {
